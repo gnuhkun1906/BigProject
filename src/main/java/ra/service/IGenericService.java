14c8cdf98e988d903a15879dfb19a79b57d1581a
Nguyencongnguyen1812 <ngannt33@wru.vn>
2023-06-27 15:40:22 +0700

    add Service

--- src/main/java/ra/service/IGenericService.java
+++ src/main/java/ra/service/IGenericService.java
@@ -1,10 +1,11 @@
 package ra.service;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IGenericService <E,T>{
-    Iterable<E> findAll();
-    Optional<E> findById(T id);
-    Optional<E> save(E e);
+    List<E> findAll();
+    E findById(T id);
+    E save(E e);
     void deleteById(T id);
 }
--- src/main/java/ra/serviceImpl/ServiceImpl.java
+++ src/main/java/ra/serviceImpl/ServiceImpl.java
@@ -6,6 +6,7 @@ import ra.model.Music;
 import ra.repository.IMusicRepository;
 import ra.service.IMusicService;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,19 +14,20 @@ public class ServiceImpl implements IMusicService {
     @Autowired
     private IMusicRepository musicRepository;
 
+
     @Override
-    public Iterable<Music> findAll() {
+    public List<Music> findAll() {
         return musicRepository.findAll();
     }
 
     @Override
-    public Optional<Music> findById(Long id) {
-        return musicRepository.findById(id);
+    public Music findById(Long id) {
+        return musicRepository.findById(id).get();
     }
 
     @Override
-    public Optional<Music> save(Music music) {
-        return Optional.of(musicRepository.save(music));
+    public Music save(Music music) {
+        return musicRepository.save(music);
     }
 
     @Override
